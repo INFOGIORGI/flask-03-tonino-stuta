c55487f918549329facfaccf4bd2cd597e7e2f01
tonino-stuta <119683972+tonino-stuta@users.noreply.github.com>
2024-12-13 11:58:59 +0000

    commit_1

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
+def home():
+    return render_template("home.html")
+@app.route("/details")
+def details():
+    return render_template("details.html")
 app.run()
